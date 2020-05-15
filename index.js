f28d48f6ede391133f5edb495b2ce57f2df8fa35
jmtomeX <jmtomeX@gmail.com>
2020-05-15 17:28:06 +0200

    Cambiado el sistema de recoger el puerto

--- index.js
+++ index.js
@@ -2,27 +2,32 @@
 "use strict";
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = proccess.env.PORT || 3000;
 // utizamos las promesas para conectarnos mediante promesas
 mongoose.Promise = global.Promise;
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 // para conectar a mongoose
 mongoose
   .connect("mongodb://localhost:27017/curso_mean_social", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log(
       "La conexión a la base de datos se ha realizado correctamente."
     );
-
+    // settings
+    // obtener el puerto si el servidor da un puerto se utiliza, si no usa el 3000
+    app.set("port", process.env.PORT || 3000);
+   
     // crear servidor
-    app.listen(port, () => {
-      console.log("Servidor corriendo correctamente");
-    });
+    // app.listen(port, () => {
+    //   console.log("Servidor corriendo correctamente");
+    // });
+    app.listen(app.get('port'), () => {
+      console.log('Servidor en puerto ', app.get('port'));
+  });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 // Hasta aquí ya se puede trabajar con lo base de datos
 
