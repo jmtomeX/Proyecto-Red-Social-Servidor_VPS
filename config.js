964dbaecced1afd1ab90d98714cb610732552599
jmtomeX <jmtomeX@gmail.com>
2020-05-16 13:24:17 +0200

    Modificado get * 17

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const config = {
-    application: {
-        cors: {
-            server: [
-                {
-                    origin: "https://coronavirusmetting.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
-                    credentials: true
-                }
-            ]
-        }
-}
+  application: {
+    cors: {
+      server: [
+        {
+          origin: "https://coronavirusmetting.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+          credentials: true,
+        },
+      ],
+    },
+  },
+};
