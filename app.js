b6fae2d1aea63d0bbe4b53f3081c081128769c69
jmtomeX <jmtomeX@gmail.com>
2020-05-16 12:39:49 +0200

    Modificado get * 17

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 //app.use(express.static(path.join(__dirname, 'client')), );
 
 // entrar a la carpeta client sin que redireccione, de producción de angular. en vez de la llamada de arriba comentada.
-app.use(express.static("client", { redirect: false }));
+app.use(express.static("client"));
 // sobreiscribe la url
 app.use("/api", user_routes);
 app.use("/api", follow_routes);
@@ -58,7 +58,7 @@ app.use("/api", messages_routes);
 
 app.get("*", function (req, res, next) {
 
-  // if (req.headers["x-forwarded-proto"] != "https") {
+     // if (req.headers["x-forwarded-proto"] != "https") {
     //   res.redirect("https://coronavirusmetting.herokuapp.com/" + req.url);
     // } else next();
     
